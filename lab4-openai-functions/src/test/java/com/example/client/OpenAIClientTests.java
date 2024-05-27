c216e669602c002d97fd0e3b52f1189679b814df
Ken Krueger <kenkrueger65@gmail.com>
2024-05-27 15:17:52 -0400

    unused import

--- lab4-openai-functions/src/test/java/com/example/client/OpenAIClientTests.java
+++ lab4-openai-functions/src/test/java/com/example/client/OpenAIClientTests.java
@@ -7,7 +7,6 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.ai.retry.NonTransientAiException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
