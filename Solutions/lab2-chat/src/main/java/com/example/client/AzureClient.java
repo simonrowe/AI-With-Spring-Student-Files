6860d31de9b630f3c6c64eca680916e67cb94a18
Ken Krueger <kenkrueger65@gmail.com>
2024-07-08 20:47:37 -0400

    unused imports

--- Solutions/lab2-chat/src/main/java/com/example/client/AzureClient.java
+++ Solutions/lab2-chat/src/main/java/com/example/client/AzureClient.java
@@ -1,13 +1,8 @@
 package com.example.client;
 
 import org.springframework.ai.azure.openai.AzureOpenAiChatModel;
-import org.springframework.ai.azure.openai.AzureOpenAiChatOptions;
-import org.springframework.ai.bedrock.titan.BedrockTitanChatModel;
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.model.ChatResponse;
-import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.ai.chat.prompt.Prompt;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
