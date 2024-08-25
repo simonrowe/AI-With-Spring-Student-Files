e8fc57242b4f8914aa665ef0ba3ebb139b9f12c3
Ken Krueger <kenkrueger65@gmail.com>
2024-08-25 08:19:23 -0400

    Double-checked state test

--- Experiments/chat-experiments/src/test/java/com/example/client/StateTests.java
+++ Experiments/chat-experiments/src/test/java/com/example/client/StateTests.java
@@ -31,49 +31,58 @@ public class StateTests {
         client = ChatClient
             .builder(model)
             .defaultAdvisors(
-                new MessageChatMemoryAdvisor (memory)//,
-                //new SimpleLoggerAdvisor()
+                new MessageChatMemoryAdvisor (memory)
             )
             .build();
 
         var conversationKey = AbstractChatMemoryAdvisor.CHAT_MEMORY_CONVERSATION_ID_KEY;
-        String response = client.prompt()
+
+        //  Conversation 1: Facts about the Great Lakes.
+        String response1 = client.prompt()
             .user("List out the names of the Great Lakes.")
             .advisors(a -> a.param(conversationKey, 111))
             .call()
             .content();
         
-        System.out.println(response);
-        // use assertJ to assert that the response contains the word "price" and "$":
-        assertThat(response).contains("Superior", "Huron", "Michigan", "Erie", "Ontario");
-   
+        System.out.println(response1);
+        // use assertJ to assert that the response contains the names of the Great Lakes:
+        assertThat(response1).contains("Superior", "Huron", "Michigan", "Erie", "Ontario");
+
+        //  Conversation 2: Facts about Florida Lakes.
+        String response2 = client.prompt()
+        .user("List out the 10 largest lakes in Florida.")
+        .advisors(a -> a.param(AbstractChatMemoryAdvisor.CHAT_MEMORY_CONVERSATION_ID_KEY, 222))
+        .call()
+        .content();
+    
+        System.out.println(response2);
+        // use assertJ to assert that the response contains the names of some Florida lakes:
+        assertThat(response2).contains("Okeechobee", "George", "Apopka");
+        
+
         //  Execute another test to investigate whether the chat client is aware of state.
-        response = client.prompt()
+        response1 = client.prompt()
         .user("Which one is the biggest by surface area?")
         .advisors(a -> a.param(AbstractChatMemoryAdvisor.CHAT_MEMORY_CONVERSATION_ID_KEY, 111))
         .call()
         .content();
         
-        System.out.println(response);
+        System.out.println(response1);
+        // use assertJ to assert that the response contains the word "Superior":
+        assertThat(response1).contains("Superior");
 
         //////////////////
-        //  SAME CLIENT, DIFFERENT CONVERSATION ID.
-        response = client.prompt()
-        .user("List out some of the best known lakes in Florida.")
-        .advisors(a -> a.param(AbstractChatMemoryAdvisor.CHAT_MEMORY_CONVERSATION_ID_KEY, 222))
-        .call()
-        .content();
-    
-        System.out.println(response);
 
-        //  Execute another test to investigate whether the chat client is aware of state.
-        response = client.prompt()
-        .user("Which one is the biggest by surface area?")
+        //  Execute another test to prove that the chat client is aware of a different conversation.
+        response2 = client.prompt()
+        .user("Which one is the deepest?")
         .advisors(a -> a.param(AbstractChatMemoryAdvisor.CHAT_MEMORY_CONVERSATION_ID_KEY, 222))
         .call()
         .content();
         
-        System.out.println(response);
+        System.out.println(response2);
+        // use assertJ to assert that the response contains the name "George":
+        assertThat(response2).contains("George");
 
     }
         
